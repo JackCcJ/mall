986412ab59670b39c14e8eaff4c965720647b936
JackAf <2350481526@qq.com>
2020-05-09 14:55:35 +0800

    首页搭建

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-
+    <router-view/>
+    <main-tab-bar/>
   </div>
 </template>
 
 <script>
+import MainTabbar from 'components/content/mainTabbar/MainTabBar';
 export default {
   name:'app',
   components: {
-    
+    MainTabBar
   }
 }
 </script>
 <style>
-  @import "assets/css/base";
+  @import "assets/css/base.css";
 </style>
