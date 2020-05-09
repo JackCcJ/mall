5880dbd8fb4713c092a1874b7ef57166bd1efe39
JackAf <2350481526@qq.com>
2020-05-09 13:34:13 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:'app',
+  components: {
+    
+  }
+}
+</script>
+<style>
+  @import "assets/css/base";
+</style>
