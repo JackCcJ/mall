306c670307db326d868dd00f540d4f806782d92b
JackAf <2350481526@qq.com>
2020-05-11 10:53:17 +0800

    bug修复

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
-  <div id="app">
+  <div id="app" class="wrapper">
     <router-view/>
+    <main-tab-bar/>
   </div>
 </template>
 
 <script>
-import MainTabbar from './components/content/mainTabbar/MainTabBar';
-export default {
-  name:'app',
-  components: {
-    MainTabBar
-  }
+  import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+
+  export default {
+    name:'app',
+    components: {
+      MainTabBar
+    }
 }
 </script>
 <style>
   @import "assets/css/base.css";
 </style>
+
