8f9d727c1a08f69c0fa2b9ecbd3735f0229baf38
JackAf <2350481526@qq.com>
2020-05-11 21:37:55 +0800

    轮播图封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/navBar';
   import {getHomeMultidata} from 'network/home';
-  
   export default {
     name: "Home",
     components:{
       NavBar
+    },
+    data(){
+      return {
+        banners:[],
+        recommends:[]
+      }
+    },
+    created(){
+      getHomeMultidata().then(res => {
+        console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
     }
   }
 </script>
