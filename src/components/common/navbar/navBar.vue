f2dcdd7709ebb859f248eda10989bd6c58806982
JackAf <2350481526@qq.com>
2020-05-11 12:05:49 +0800

    头部公用组件增加

---
+++ src/components/common/navbar/navBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <slot></slot>
+      <slot></slot>
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+ name:"navBar"
+}
+</script>
+
+<style>
+
+</style>
