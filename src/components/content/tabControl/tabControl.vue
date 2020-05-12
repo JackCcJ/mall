a652f940e9b9cf7a915453b413b179ba23f9c97f
JackAf <2350481526@qq.com>
2020-05-12 17:59:01 +0800

    分类

---
+++ src/components/content/tabControl/tabControl.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="tab-control">
+      <div v-for="(item, index) in titles" class="tab-control-item" :key="item.id" :class="{active : index === currentIndex}" @click="itemclick(index)">
+          <span>
+              {{item}}
+          </span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name :"tabControl",
+    props:{
+        titles:{
+            type:Array,
+            default(){
+                return []
+            }
+        }
+    },data(){
+        return{
+            currentIndex:0
+        }
+    },methods:{
+            itemclick(index){
+                this.currentIndex = index
+            }
+    }
+}
+</script>
+
+<style>
+    .tab-control{
+       display: flex;
+       text-align: center;
+    }
+    .tab-control-item{
+        flex: 1;
+        height: 40px;
+        line-height: 40px;
+    }
+    .tab-control-item span{
+        padding: 5px;
+    }
+    .active{
+        color: var(--color-high-text);
+    }
+    .active span{
+        border-bottom: 3px solid var(--color-high-text)
+    }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,22 +3,31 @@
     <nav-bar class="home-nav"><div slot="center">风羽购物商城</div></nav-bar>
     <home-swiper :banners="banners" />
     <recommend-view :recommends="recommends" />
+    <feature-view />
+    <tab-control class="tab-control" :titles="['流行','新款','精选']" />
   </div>
 </template>
 
 <script>
+  //公共组件
   import NavBar from 'components/common/navbar/navBar';
+  import TabControl from 'components/content/tabControl/tabControl';
+  //网络请求组件
   import {getHomeMultidata} from 'network/home';
+  //子组件
   import HomeSwiper from './childComps/HomeSwiper';
   import RecommendView from './childComps/RecommendView'
-  
+  import FeatureView from './childComps/FeatureView'
+
   export default {
     name: "Home",
     components:{
       NavBar,
+      TabControl,
+      getHomeMultidata,
       HomeSwiper,
       RecommendView,
-      getHomeMultidata
+      FeatureView
     },
     data(){
       return {
@@ -36,8 +45,22 @@
 </script>
 
 <style scoped>
+  #home{
+    padding-top: 44px;
+  }
  .home-nav{
    color: #fff;
    background: var(--color-tint);
+   top: 0;
+   left: 0;
+   position: fixed;
+   width: 100%;
+   z-index: 99;
+   height: 44px;
+ }
+ .tab-control{
+   background-color: #fff;
+   position: sticky;
+   top: 43px;
  }
 </style>
